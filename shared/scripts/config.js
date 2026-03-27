434ee6a382d38edd58840131793a08a3910cddbc
Yunus <muhammedyunusamini4@gmail.com>
2026-03-27 01:00:30 +0100

    init coderr frontend clean

---
+++ shared/scripts/config.js
@@ -0,0 +1,38 @@
+const GUEST_LOGINS = {
+    customer : {
+        username: 'andrey',
+        password: 'asdasd'
+    },
+    business : {
+        username: 'kevin',
+        password: 'asdasd24'
+    }
+}
+
+const API_BASE_URL = 'https://coderr-backend-1ons.onrender.com/api/';
+const STATIC_BASE_URL = 'https://coderr-backend-1ons.onrender.com/';
+
+const LOGIN_URL = 'login/';
+
+const REGISTER_URL = 'registration/';
+
+const PROFILE_URL = 'profile/';
+
+const BUSINESS_PROFILES_URL = 'profiles/business/';
+
+const CUSTOMER_PROFILES_URL = 'profiles/customer/';
+
+const REVIEW_URL = 'reviews/';
+
+const ORDER_URL = 'orders/';
+
+const OFFER_URL = 'offers/';
+
+const OFFER_DETAIL_URL = 'offerdetails/';
+
+const BASE_INFO_URL = 'base-info/';
+
+const OFFER_INPROGRESS_COUNT_URL = 'order-count/';
+const OFFER_COMPLETED_COUNT_URL = 'completed-order-count/';
+
+const PAGE_SIZE = 6
